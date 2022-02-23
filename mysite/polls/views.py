3898c38459e5ffc465ff03bb9fcfd6df3bcfb6a8
mutotu <muttalip.aydemir@gmail.com>
2022-02-23 11:21:48 -0800

    adding css

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -50,30 +50,28 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-    
+
     def get_queryset(self):
-                """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte)=timezone.now()
+
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
     
-def vote(request, question_id):
-    # return HttpResponse("You're voting on question {}".format(question_id))
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question':question,
-            'error_message':"You didn't select a choice"})
+    def vote(request, question_id):
 
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-    
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            return render(request, 'polls/detail.html', {
+                'question':question,
+                'error_message':"You didn't select a choice"})
+
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        
